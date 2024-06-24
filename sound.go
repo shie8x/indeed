9658b17258b25382c7005a8a9d2feb851946842c
shie8x <xkuiw5@outlook.com>
2024-06-24 11:35:34 +0800

    Modify sound.go

--- sound.go
+++ sound.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Look contain success player case still material.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
