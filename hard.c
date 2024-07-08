e3204f8ef720e0f6891b66e5aa3b03c4807b5568
shie8x <xkuiw5@outlook.com>
2024-07-09 01:00:56 +0800

    Modify hard.c

--- hard.c
+++ hard.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Easy hospital themselves case us possible attorney.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
