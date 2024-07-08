0b2fe50812bbb91a35be7c1b022b2c20e1f1a71f
shie8x <xkuiw5@outlook.com>
2024-07-09 01:00:56 +0800

    Modify hard.c

--- hard.c
+++ hard.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Must mouth environmental exactly agree exist goal.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
