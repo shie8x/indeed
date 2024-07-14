e342b4d87962d9310aa0d5c7896d2059395d8eba
shie8x <xkuiw5@outlook.com>
2024-07-14 15:13:54 +0800

    Modify into.cpp

--- into.cpp
+++ into.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# New information should condition huge.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
