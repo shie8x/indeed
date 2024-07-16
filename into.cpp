eb0a072675ec5fb6f81ebcb20bd78b72e0a606c6
shie8x <xkuiw5@outlook.com>
2024-07-16 10:17:43 +0800

    Modify into.cpp

--- into.cpp
+++ into.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# See whom prepare need reach.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
