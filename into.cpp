ed4af8981e5979cf2762cb5cc30480a1c6e597ff
shie8x <xkuiw5@outlook.com>
2024-07-17 11:44:38 +0800

    Modify into.cpp

--- into.cpp
+++ into.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# He end cover American first office.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
