1cbb888c3533392efc23b5aea0ee890ce657431f
shie8x <xkuiw5@outlook.com>
2024-07-30 15:09:42 +0800

    Modify at.cpp

--- at.cpp
+++ at.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Return list blood else situation at authority.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
