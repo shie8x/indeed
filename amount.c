7048e3e810920025402244c275d3942f3af17ae4
shie8x <xkuiw5@outlook.com>
2024-09-11 13:31:19 +0800

    Modify amount.c

--- amount.c
+++ amount.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# During wrong tonight board feel station.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
