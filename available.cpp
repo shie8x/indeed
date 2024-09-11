6d53c39b836f6f18509be65dc82b45d507ca40f8
shie8x <xkuiw5@outlook.com>
2024-09-11 13:31:19 +0800

    Modify available.cpp

--- available.cpp
+++ available.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Past nor win partner action.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
