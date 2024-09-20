d90aba5bd89931f106248aef287cf9a3fdd28a2a
shie8x <xkuiw5@outlook.com>
2024-09-20 19:43:13 +0800

    Modify head.cpp

--- head.cpp
+++ head.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Break executive friend president worry consider.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
