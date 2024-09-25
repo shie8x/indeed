ec7d86922f43a613d5a1dbbacf3d96867b8c4ea0
shie8x <xkuiw5@outlook.com>
2024-09-25 11:30:08 +0800

    Modify manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Push scientist could choose PM.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
