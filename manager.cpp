5aee2ef13ac713f3ae7055c44c855a8e6a4add3c
shie8x <xkuiw5@outlook.com>
2024-09-27 12:45:25 +0800

    Modify manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# This skill PM late professional.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
