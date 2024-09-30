792a162e8a20f6c1cf85e56df6adfeb07d8c387c
shie8x <xkuiw5@outlook.com>
2024-09-30 13:29:38 +0800

    Modify small.cpp

--- small.cpp
+++ small.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Voice article draw together report value.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
