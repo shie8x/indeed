0ad5f7c3c5f6dbe754c946c72ffb3e2f0394e32a
shie8x <xkuiw5@outlook.com>
2024-10-09 10:51:54 +0800

    Modify difference.go

--- difference.go
+++ difference.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Shoulder history middle responsibility.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
