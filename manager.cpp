9b7703e86ecd95e51b6079ccac9e0672f013bf61
shie8x <xkuiw5@outlook.com>
2024-10-12 10:06:39 +0800

    Modify manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Yard least fall mother poor with.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
