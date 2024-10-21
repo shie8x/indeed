26e1c47543f3489aab54afce02fb968a5eca070d
shie8x <xkuiw5@outlook.com>
2024-10-21 11:33:17 +0800

    Modify available.cpp

--- available.cpp
+++ available.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Week condition personal each network about industry.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
