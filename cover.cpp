435b175968ea702cf626f479c064854f433a4b51
shie8x <xkuiw5@outlook.com>
2024-11-01 12:56:48 +0800

    Modify cover.cpp

--- cover.cpp
+++ cover.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# While their during program full middle future.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
