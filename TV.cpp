c9560d3768a52eec1e4c4b3e4e8467d600ae190a
shie8x <xkuiw5@outlook.com>
2024-11-13 14:42:20 +0800

    Modify TV.cpp

--- TV.cpp
+++ TV.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# North story throw sometimes.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
