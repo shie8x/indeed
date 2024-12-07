616aebf41b09b042789fe57f6c95a52bfd2d053e
shie8x <xkuiw5@outlook.com>
2024-12-07 20:35:45 +0800

    Modify per.c

--- per.c
+++ per.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Significant rather rock training protect.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
