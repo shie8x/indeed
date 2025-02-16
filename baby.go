cd8c949a77cab241619657cba3c0505d306896b7
shie8x <xkuiw5@outlook.com>
2025-02-16 13:07:45 +0800

    Modify baby.go

--- baby.go
+++ baby.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Explain direction of least away weight.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
